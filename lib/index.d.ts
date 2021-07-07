5cb0f9f83fc6ac5a0637be880e0404f3ae69f36b
FuWenHao <fuwenhao@acoinfo.com>
2021-07-07 20:28:16 +0800

    add index.d.ts

---
+++ lib/index.d.ts
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) 2021 EdgerOS Team.
+ * All rights reserved.
+ * 
+ * Detailed license information can be found in the LICENSE file.
+ * 
+ * Author       : Fu Wenhao <fuwenhao@acoinfo>
+ * Date         : 2021-07-07 19:37:16
+ * LastEditors  : Fu Wenhao <fuwenhao@acoinfo>
+ * LastEditTime : 2021-07-07 20:24:57
+ */
+
+
+declare module selintPluginJsre {
+  let rules: any;
+  let processors: any;
+  let environments: any;
+}
+
+export = selintPluginJsre
